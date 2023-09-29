42c904aa930e3a48138d9287acfbc6376373541f
Paula <paula.cielava1998@gmail.com>
2023-09-29 15:21:26 +0300

    kkas

--- src/main/java/lv/venta/confs/SecurityConfig.java
+++ src/main/java/lv/venta/confs/SecurityConfig.java
@@ -2,80 +2,93 @@ package lv.venta.confs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import lv.venta.models.security.MyAuthority;
+import lv.venta.services.impl.security.MyUserDetailsManagerImpl;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
-	}
-	
+  
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/"));
+  }
 
-	// nodoršināt lietotājus un to lomas (lietotājvārdi, paroles)
-	//userDetailsManager
-	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		UserDetails user1 = User.withDefaultPasswordEncoder().username("paula.cielava").password("123").authorities("ADMIN").build();
-		manager.createUser(user1);
-		
-		UserDetails user2 = User.withDefaultPasswordEncoder().username("janis.berzins").password("321").authorities("USER").build();
-		manager.createUser(user2);
-		UserDetails user3 = User.withDefaultPasswordEncoder().username("liga.jauka").password("111").authorities("USER", "ADMIN").build();
-		manager.createUser(user3);
-		
-		return manager;
-	}
-	
-	
-	
-	// modrošināt piekļuvi konkrētiem endpointiem
-	//simpleSecurityChain
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-		http.authorizeHttpRequests()
-		.requestMatchers("/hello").permitAll()
-		.requestMatchers("/msg").permitAll()
-		.requestMatchers("/product").hasAnyAuthority("ADMIN")
-		.requestMatchers("/productOne").hasAnyAuthority("ADMIN")
-		.requestMatchers("/product/**").hasAnyAuthority("ADMIN", "USER")
-		.requestMatchers("/allproducts").permitAll()
-		.requestMatchers("/allproducts/**").permitAll()
-		.requestMatchers("/insert").hasAnyAuthority("ADMIN")
-		.requestMatchers("/update/**").hasAnyAuthority("ADMIN")
-		.requestMatchers("/delete/**").hasAnyAuthority("ADMIN")
-		.requestMatchers("/filter/quantity/**").permitAll()
-		.requestMatchers("/error").permitAll()
-		.requestMatchers("/h2-console").permitAll()
-		.requestMatchers("/h2-console/**").permitAll()
-		.and()
-		.formLogin().permitAll()
-		.and()
-		.logout().permitAll();
-		
-		return http.build();
-		
-		
-		// 1. izveidot myUser klasi ar 
-		//    id, name, surmane, username, password
-		// 2. izveidot Authority klasi ar id, title
-		// 3. uztaisīt starp abām saistību - manyToMany
-		// 4. izveidot klasē ar maind funkciju arī ComandLinerRunner 
-		//    funkciju un tajā ievietot testa datus
-		// 5. veidosim savu MyUserDetails
-		// 6. veidosim savu MyUserDetailsImpl
-	}
-	
-	
+  //norošināt lietotajus un to lomas
+  //userDetailsManager
+  /* @Bean
+  public InMemoryUserDetailsManager userDetailsManager() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(User.withDefaultPasswordEncoder()
+    .username("alina.rubcova").password("123").authorities("ADMIN").build());
+    manager.createUser(User.withDefaultPasswordEncoder()
+    .username("janis.berzins").password("321").authorities("USER").build());
+    manager.createUser(User.withDefaultPasswordEncoder()
+    .username("liga.jauka").password("987").authorities("USER", "ADMIN").build());
+  
+    return manager;
+  }
+  */
+  
+  @Bean
+  public MyUserDetailsManagerImpl userDetailsManager() {
+    MyUserDetailsManagerImpl manager = new MyUserDetailsManagerImpl();
+    return manager;
+  }
+  
+  @Bean 
+  public PasswordEncoder passwordEncoderSimple2() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+  
+  @Bean
+  public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+  AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+  
+  
+  authenticationManagerBuilder.userDetailsService(userDetailsManager()).passwordEncoder(passwordEncoderSimple2());
+  return authenticationManagerBuilder.build();
+  }
+  
+  
+  //nodorošināt piekļuvi konkrētiem endpointien
+  //SecurityFilterChain
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    http.authorizeHttpRequests()
+    .requestMatchers("/hello").permitAll()
+    .requestMatchers("/msg").permitAll()
+    .requestMatchers("/product").hasAnyAuthority("ADMIN")
+    .requestMatchers("/productOne").hasAnyAuthority("ADMIN")
+    .requestMatchers("/product/").hasAnyAuthority("ADMIN", "USER")
+    .requestMatchers("/allproducts").permitAll()
+    .requestMatchers("/allproducts/").permitAll()
+    .requestMatchers("/insert").hasAnyAuthority("ADMIN")
+    .requestMatchers("/update/").hasAnyAuthority("ADMIN")
+    .requestMatchers("/delete/").hasAnyAuthority("ADMIN")
+    .requestMatchers("/filter/quantity/").permitAll()
+    .requestMatchers("/error").permitAll()
+    .requestMatchers("/h2-console").permitAll()
+    .requestMatchers("/h2-console/**").permitAll()
+    .and()
+    .formLogin().permitAll()
+    .and()
+    .logout().permitAll();
+    
+    return http.build();
+
+  }
+
 }
